16d48f67ea510a4f83e93ffff70a8f78efe289c6
kushal-p1699 <kushal.p1699@gmail.com>
2021-07-03 20:32:55 +0530

    layout tabs added to main activity

--- app/src/main/java/com/example/covidsetu/MainActivity.java
+++ app/src/main/java/com/example/covidsetu/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.covidsetu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabItem1, tabItem2, tabItem3;
+    ViewPager viewPager;
+
+    PageAdapater pageAdapater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout_vaccination);
+        tabItem1 = (TabItem) findViewById(R.id.tab_item1);
+        tabItem2 = (TabItem) findViewById(R.id.tab_item2);
+        tabItem3 = (TabItem) findViewById(R.id.tab_item3);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager_id);
+
+        pageAdapater = new PageAdapater(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapater);
+
+        // listen for tab click
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if(tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2){
+                    pageAdapater.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        // listen for new scroll or page change
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
     }
 }
---
+++ app/src/main/java/com/example/covidsetu/PageAdapater.java
@@ -0,0 +1,32 @@
+package com.example.covidsetu;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapater extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public PageAdapater(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabCount = behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0 : return new VaccinationFragment();
+            case 1 : return new FragmentItem2();
+            case 2 : return new FragmentItem3();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
